fcdfac70c27b5bc9312bd837569c638c3dc528c4
Pavani <durga.k@apssdc.in>
2020-04-29 10:12:18 +0530

    Updates

---
+++ precache-manifest.e510932d0bec9a74ea69a83b5ebc0b48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584e200f6fc850963b7af77beb81347e",
+    "url": "/myproject/index.html"
+  },
+  {
+    "revision": "2431ad3b3ea90fad35c2",
+    "url": "/myproject/static/css/main.722efffb.chunk.css"
+  },
+  {
+    "revision": "5cb5dab094fafa9e222c",
+    "url": "/myproject/static/js/2.bc8e1bff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myproject/static/js/2.bc8e1bff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2431ad3b3ea90fad35c2",
+    "url": "/myproject/static/js/main.ee49c4d5.chunk.js"
+  },
+  {
+    "revision": "154c45877ad0ac95d280",
+    "url": "/myproject/static/js/runtime-main.f115114f.js"
+  }
+]);
